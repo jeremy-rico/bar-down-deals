694d773cd962f49b758c7f79bda2052a66ff9d77
Jeremy Rico <jeremy.rico35@gmail.com>
2025-02-25 18:15:27 -0800

    update navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,17 +4,13 @@ import { navigation } from "@/constants/index.tsx";
 
 export default function NavBar() {
   const items = navigation.map((item) => (
-    <li key={item.title}>
+    <li key={item.title} className="px-4 py-2 rounded hover:bg-gray-800">
       <Link href={item.href}>{item.title}</Link>
     </li>
   ));
   return (
-    <div className="hidden md:block max-w-screen-2xl mx-auto py-3">
-      <nav>
-        <ul className="flex justify-evenly w-full text-white text-lg">
-          {items}
-        </ul>
-      </nav>
-    </div>
+    <nav className="hidden md:block max-w-screen-2xl mx-auto py-1">
+      <ul className="flex justify-evenly w-full text-white text-lg">{items}</ul>
+    </nav>
   );
 }
