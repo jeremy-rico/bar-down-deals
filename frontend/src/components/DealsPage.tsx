6264336485e469dc173236e7cf53883fdd167669
Jeremy Rico <jeremy.rico35@gmail.com>
2025-03-25 18:20:17 -0700

    working filtering LFG

--- frontend/src/components/DealsPage.tsx
+++ frontend/src/components/DealsPage.tsx
@@ -4,33 +4,87 @@ import SortMenu from "@/components/SortMenu.tsx";
 import FilterOptions from "@/components/FilterOptions.tsx";
 import Pagination from "@/components/Pagination.tsx";
 import {} from "@heroicons/react/24/outline";
-import { api } from "@/constants/index.tsx";
 import { useState, useEffect } from "react";
 
-export default function DealsPage({ title, sort, filterOptions }) {
+export default function DealsPage({ baseQuery, title, sort }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState("");
-  const [deals, setDeals] = useState([]);
+  const [totalPages, setTotalPages] = useState();
+  const [totalItems, setTotalItems] = useState(0);
   const [sortOption, setSortOption] = useState(sort);
-  const [selectedFilters, setSelectedFilters] = useState([]);
+
+  const [filterOptions, setFilterOptions] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState({
+    tags: [],
+    brands: [],
+    stores: [],
+  });
+  const [maxAvailPrice, setMaxAvailPrice] = useState();
+
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState();
+
+  const [deals, setDeals] = useState([]);
 
   useEffect(() => {
     async function fetchDeals() {
-      const query = new URLSearchParams(); // TODO: let instead of const?
+      // Create query params
+      const query = new URLSearchParams();
+      query.append("page", currentPage);
       if (sortOption.sort) query.append("sort", sortOption.sort);
       if (sortOption.order) query.append("order", sortOption.order);
-      query.append("page", currentPage);
+      if (minPrice) query.append("min_price", minPrice);
+      if (maxPrice) query.append("max_price", maxPrice);
+
+      // Iterate through selected filters
       Object.entries(selectedFilters).forEach(([key, value]) => {
-        if (value) query.append(key, value);
+        for (let i = 0; i < value.length; i++) {
+          query.append(key, value[i]);
+        }
       });
 
-      const response = await fetch(api + `/deals/?${query.toString()}`);
+      // Get pagination and filtering info from headers
+      const response = await fetch(baseQuery + `${query.toString()}`);
       setTotalPages(response.headers.get("x-total-page-count"));
+      setTotalItems(response.headers.get("x-total-item-count"));
+      setMaxAvailPrice(response.headers.get("x-max-price"));
+
+      const filters = [];
+      if (response.headers.get("x-avail-sizes")) {
+        filters.push({
+          title: "Size",
+          query: "tags",
+          options: JSON.parse(response.headers.get("x-avail-sizes")),
+        });
+      }
+      if (response.headers.get("x-avail-brands")) {
+        filters.push({
+          title: "Brand",
+          query: "brands",
+          options: JSON.parse(response.headers.get("x-avail-brands")),
+        });
+      }
+      if (response.headers.get("x-avail-stores")) {
+        filters.push({
+          title: "Store",
+          query: "stores",
+          options: JSON.parse(response.headers.get("x-avail-stores")),
+        });
+      }
+      if (response.headers.get("x-avail-tags")) {
+        filters.push({
+          title: "Tags",
+          query: "tags",
+          options: JSON.parse(response.headers.get("x-avail-tags")),
+        });
+      }
+      setFilterOptions(filters);
+
+      // Get deals from response content
       const data = await response.json();
       setDeals(data);
     }
     fetchDeals();
-  }, [sortOption, selectedFilters, currentPage]);
+  }, [baseQuery, sortOption, selectedFilters, currentPage, minPrice, maxPrice]);
 
   return (
     <div className="my-4">
@@ -41,6 +95,9 @@ export default function DealsPage({ title, sort, filterOptions }) {
           filterOptions={filterOptions}
           selectedFilters={selectedFilters}
           onFilterChange={setSelectedFilters}
+          onMinPriceChange={setMinPrice}
+          maxAvailPrice={maxAvailPrice}
+          onMaxPriceChange={setMaxPrice}
         />
         <div className="grid grid-rows-1 grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
           {deals.map((deal) => (
@@ -48,7 +105,10 @@ export default function DealsPage({ title, sort, filterOptions }) {
           ))}
         </div>
       </div>
-      <div className="flex justify-end">
+      <div className="flex justify-between items-center">
+        <p className="block text-sm text-gray-500 ml-4">
+          {totalItems} deals found
+        </p>
         <Pagination
           onPageChange={setCurrentPage}
           currentPage={currentPage}
--- frontend/src/components/FilterOptions.tsx
+++ frontend/src/components/FilterOptions.tsx
@@ -1,70 +1,54 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { Input, Checkbox, Field, Label } from "@headlessui/react";
+import { Checkbox, Field, Label } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/24/outline";
+import PriceRange from "@/components/PriceRange.tsx";
 
-export default function FilterList({
+export default function FilterOptions({
   filterOptions,
   selectedFilters,
   onFilterChange,
+  onMinPriceChange,
+  maxAvailPrice,
+  onMaxPriceChange,
 }) {
-  function handleToggleFilter(filterId, value) {
-    setSelectedFilters((prevFilters) => ({
-      ...prevFilters,
-      [filterId]: value,
+  const toggleFilter = (query, option) => {
+    onFilterChange((prev) => ({
+      ...prev, // Keep the rest of the filters
+      [query]: prev[query]?.includes(option)
+        ? prev[query].filter((item) => item !== option) // Remove if exists
+        : [...(prev[query] || []), option], // Add if not exists
     }));
-  }
-
-  useEffect(() => {
-    onFilterChange(selectedFilters);
-  }, [selectedFilters, onFilterChange]);
+  };
 
   return (
     <div className="flex-grow max-w-56">
       {filterOptions.map((filter, index) => (
         <div key={index} className="">
           <div>
-            <h1 className="bg-black text-white py-2 pl-2">{filter.label}</h1>
+            <h1 className="bg-black text-white py-2 pl-2">{filter.title}</h1>
           </div>
           <div className="flex flex-col gap-y-2 my-4">
-            {filter.children.map((child, child_index) => (
-              <Field key={child_index} className="flex items-center">
+            {filter.options.map((option, optionIndex) => (
+              <Field key={optionIndex} className="flex items-center">
                 <Checkbox
-                  checked={child.enabled}
-                  onChange={() => handleToggleFilter()}
+                  checked={selectedFilters[filter.query].includes(option)}
+                  onChange={() => toggleFilter(filter.query, option)}
                   className="group size-5 border border-gray-800 rounded-sm"
                 >
                   <CheckIcon className="hidden size-5 group-data-[checked]:block" />
                 </Checkbox>
-                <Label className="ml-2">{child.label}</Label>
+                <Label className="ml-2">{option}</Label>
               </Field>
             ))}
           </div>
         </div>
       ))}
-      <div className="">
-        <h1 className="bg-black text-white py-2 pl-2">Price Range</h1>
-        <div className="flex justify-between items-center mt-2">
-          <p className="text-sm">$</p>
-          <Input
-            name="minPrice"
-            type="number"
-            min="1"
-            placeholder="Min"
-            className="w-20 rounded-sm py-1 px-1"
-          />
-          -<p className="text-sm">$</p>
-          <Input
-            name="maxPrice"
-            type="number"
-            min="1"
-            placeholder="Max"
-            className="w-20 rounded-sm py-1 px-1"
-          />
-        </div>
-      </div>
-      {selectedFilters.map((selected) => ({ selected }))}
+      <PriceRange
+        onMinPriceChange={onMinPriceChange}
+        maxPrice={maxAvailPrice}
+        onMaxPriceChange={onMaxPriceChange}
+      />
     </div>
   );
 }
